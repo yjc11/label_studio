74b475768f240d6ec411df45274addeb11e82f6f
youjiachen <youmcgrady@126.com>
2023-05-26 10:56:00 +0800

    去重代码更新

--- utils/file_func.py
+++ utils/file_func.py
@@ -1,8 +1,12 @@
 import os
 import shutil
 import random
+import imagehash
+import hashlib
 
 from pathlib import Path
+from PIL import Image
+from tqdm import tqdm
 
 
 def check_folder(folder):
@@ -35,8 +39,36 @@ def sample_files(source_dir, target_dir, sampling_rate):
         shutil.copyfile(source_file_path, target_file_path)
 
 
+def get_md5(file):
+    file = open(file, 'rb')
+    md5 = hashlib.md5(file.read())
+    file.close()
+    md5_values = md5.hexdigest()
+    return md5_values
+
+
+def get_img_hash(file):
+    with Image.open(file) as img:
+        hash = imagehash.phash(img)
+
+    return hash
+
+
 if __name__ == "__main__":
-    src = '/Users/youjiachen/Desktop/projects/label_studio_mgr/workspace/gouxuankuang/test/check_img'
-    dst = '/Users/youjiachen/Desktop/projects/label_studio_mgr/workspace/gouxuankuang/test/sample_100'
-    check_folder(dst)
-    sample_files(src, dst, 0.1)
+    # src = '/Users/youjiachen/Desktop/projects/label_studio_mgr/workspace/gouxuankuang/test/check_img'
+    # dst = '/Users/youjiachen/Desktop/projects/label_studio_mgr/workspace/gouxuankuang/test/sample_100'
+    # check_folder(dst)
+    # sample_files(src, dst, 0.1)
+
+    src = '/Users/youjiachen/Downloads/兰州模板2_1808'
+    filenames = list(Path(src).glob('**/[!.]*'))
+    # 读取图片并计算哈希值
+    hashes = list()
+    for filename in tqdm(filenames):
+        if filename.is_file():
+            hash = get_md5(filename)
+            if hash in hashes:
+                filename.unlink()
+                print(f'del {filename.name}')
+            else:
+                hashes.append(hash)
