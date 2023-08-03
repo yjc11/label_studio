13f1bbb52d943ac81711ffe047ab8b81bc30dfb0
yjc11 <youmcgrady@126.com>
2023-08-03 12:00:42 +0000

    update

--- utils/ocr_func.py
+++ utils/ocr_func.py
@@ -8,14 +8,18 @@ import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from multiprocessing.dummy import Pool as ThreadPool  # 多线程
 from pathlib import Path
-from typing import Any, Callable, List, Sequence
+from typing import Any, Callable, List, Sequence, Union
 
+import cv2
 import numpy as np
 import requests
 from tqdm import tqdm
 
-IP_ADDRESS = '192.168.106.133'
-PORT = 8506
+IP_ADDRESS = '192.168.106.12'
+PORT = 40058
+SERVER = 'http://192.168.106.12:40058'
+
+IMAGE_EXT = 'jpg|jpeg|bmp|png|tif|tiff|JPG|PNG|TIF|TIFF'
 
 
 def threaded(func: Callable):
@@ -37,6 +41,17 @@ def check_folder(folder):
     else:
         shutil.rmtree(folder)
         os.makedirs(folder)
+
+
+def convert_b64(file):
+    if os.path.isfile(file):
+        with open(file, 'rb') as fh:
+            x = base64.b64encode(fh.read())
+            return x.decode('ascii').replace('\n', '')
+    elif isinstance(file, np.ndarray):
+        bytes_data = cv2.imencode('.jpg', file)[1].tobytes()
+        b64enc = base64.b64encode(bytes_data).decode()
+        return b64enc
 
 
 def list_image(directory, ext='jpg|jpeg|bmp|png|tif|tiff|JPG|PNG|TIF|TIFF'):
@@ -64,104 +79,97 @@ def general(data, ip_address, port):
     return r.json()
 
 
-def multithreadpost(image_files, func, max_workers=1):
-    all_start_time = time.time()
-    pool = ThreadPool(max_workers)
-    results = pool.imap(func, image_files)
-    progress_bar = tqdm(results, total=len(image_files))
-    for result in progress_bar:
-        pass
-    pool.close()
-    pool.join()
-    all_end_time = time.time()
-    print('finish_time: {}'.format(all_end_time - all_start_time))
-
-
-def convert_b64(file):
-    if os.path.isfile(file):
-        with open(file, 'rb') as fh:
-            x = base64.b64encode(fh.read())
-            return x.decode('ascii').replace('\n', '')
-    else:
-        return None
-
-
-def save_to_json(output_path):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
-            image_file = args[0]
-            filename = Path(image_file).stem
-            filepath = Path(output_path) / f"{filename}.json"
-            with open(filepath, "w") as f:
-                json.dump(result, f, ensure_ascii=False, indent=2)
-            return result
-
-        return wrapper
-
-    return decorator
-
-
-def get_ocr_results(image_file, ip_address=IP_ADDRESS, port=PORT):
-    # 配置文件 /root/socr_data/socr_models/server_settings.yaml
-    # scene 这里要和yaml中的models内的name一致
-    data = {
-        'scene': 'chinese_print',
-        'image': convert_b64(image_file),
-        # 'image': image_file,
-        'parameters': {
-            'rotateupright': True,
-            'refine_boxes': True,
-            'sort_filter_boxes': True,
-            'support_long_rotate_dense': False,
-            'vis_flag': False,
-            'sdk': True,
-            'det': 'mrcnn-v5.1',
-            'recog': 'transformer-v2.8-gamma-faster',
-        },
-    }
-
-    ret = general(data, ip_address, port)
-
-    return ret['data']['json']['general_ocr_res']
-
-
 @threaded
-def get_ocr_results_and_save(image_file, output_dir):
-    """
-    结构化OCR全文识别结果配置
-    """
+def threaded_api_call(
+    image_file: Union[str, np.ndarray],
+    det_model='mrcnn-v5.1',
+    reg_model='transformer-v2.8-gamma-faster',
+    scene='chinese_print',
+    output_dir=None,
+):
+    assert det_model in ['general_text_det_mrcnn_v1.0', 'mrcnn-v5.1']
+    assert reg_model in [
+        'transformer-v2.8-gamma-faster',
+        'transformer-blank-v0.2-faster',
+        'transformer-hand-v1.16-faster',
+    ]
+
     data = {
-        'scene': 'chinese_print',
+        'scene': scene,
         'image': convert_b64(image_file),
         'parameters': {
+            'det': det_model,
+            'recog': reg_model,
             'rotateupright': True,
             'refine_boxes': True,
             'sort_filter_boxes': True,
             'support_long_rotate_dense': False,
             'vis_flag': False,
             'sdk': True,
-            'det': 'mrcnn-v5.1',
-            # 'recog': 'transformer-v2.8-gamma-faster',
+            'checkbox': ['std_checkbox'],
         },
     }
 
     ret = general(data, IP_ADDRESS, PORT)
     ocr_result = ret['data']['json']['general_ocr_res']
 
-    if output_dir:
+    if output_dir and not isinstance(image_file, np.ndarray):
         json_file = Path(image_file).stem + '.json'
         output_file = Path(output_dir) / json_file
         with open(output_file, 'w') as f:
             json.dump(ocr_result, f, ensure_ascii=False, indent=2)
+    return ocr_result
 
+
+def api_call(
+    image_file: Union[str, np.ndarray],
+    det_model='mrcnn-v5.1',
+    reg_model='transformer-v2.8-gamma-faster',
+    scene='chinese_print',
+    output_dir=None,
+):
+    assert det_model in ['general_text_det_mrcnn_v1.0', 'mrcnn-v5.1']
+    assert reg_model in [
+        'transformer-v2.8-gamma-faster',
+        'transformer-blank-v0.2-faster',
+        'transformer-hand-v1.16-faster',
+    ]
+
+    data = {
+        'scene': scene,
+        'image': convert_b64(image_file),
+        'parameters': {
+            'det': det_model,
+            'recog': reg_model,
+            'rotateupright': True,
+            'refine_boxes': True,
+            'sort_filter_boxes': True,
+            'support_long_rotate_dense': False,
+            'vis_flag': False,
+            'sdk': True,
+            'checkbox': ['std_checkbox'],
+        },
+    }
+
+    ret = general(data, IP_ADDRESS, PORT)
+    ocr_result = ret['data']['json']['general_ocr_res']
+
+    if output_dir and not isinstance(image_file, np.ndarray):
+        json_file = Path(image_file).stem + '.json'
+        output_file = Path(output_dir) / json_file
+        with open(output_file, 'w') as f:
+            json.dump(ocr_result, f, ensure_ascii=False, indent=2)
     return ocr_result
 
 
 if __name__ == '__main__':
-    input_images = '/Users/youjiachen/Desktop/长文档5个场景/公文/公文-南网项目发文'
-    output_dir = '/Users/youjiachen/Desktop/长文档5个场景/公文_dataelem_ocr_res'
+    input_images = '/mnt/disk0/youjiachen/workspace/拆迁安置协议'
+    output_dir = '/mnt/disk0/youjiachen/workspace/拆迁安置协议dataelem_ocr_res'
     check_folder(output_dir)
 
+    # 多线程
     image_files = list(Path(input_images).glob('[!.]*'))
-    get_ocr_results_and_save(image_files, output_dir=output_dir)
+    print(image_files)
+    # threaded_api_call(image_files, output_dir=output_dir)
+
+    # 处理单个文件
